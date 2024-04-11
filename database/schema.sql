610d09f4d09eb2b9ddf807d48716a0c896e40e1d
thibautRV <125503955+thibautRV@users.noreply.github.com>
2024-04-11 11:07:03 +0200

    schema.sql

---
+++ database/schema.sql
@@ -0,0 +1,16 @@
+-- Database: Numeric Farm
+
+-- DROP DATABASE IF EXISTS "Numeric Farm";
+
+CREATE DATABASE "Numeric Farm"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'fr_FR.UTF-8'
+    LC_CTYPE = 'fr_FR.UTF-8'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+
